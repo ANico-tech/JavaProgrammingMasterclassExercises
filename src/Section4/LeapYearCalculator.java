086ef3bd9f1d19a50cc048a27f6eefad79cbf304
ANico-tech <59415915+ANico-tech@users.noreply.github.com>
2020-01-01 19:55:59 +0200

    Section 4 exercises

---
+++ src/Section4/LeapYearCalculator.java
@@ -0,0 +1,57 @@
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        System.out.println("Not Leap Years");
+        System.out.println(isLeapYear2(1924));
+        System.out.println(isLeapYear(1800));
+        System.out.println(isLeapYear(1900));
+        System.out.println(isLeapYear(2100));
+        System.out.println(isLeapYear(2200));
+        System.out.println(isLeapYear(2300));
+        System.out.println(isLeapYear(2500));
+        System.out.println(isLeapYear(2600));
+        System.out.println("Leap Year");
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(2400));
+        System.out.println("TestCases");
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static boolean isLeapYear2(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else {
+            if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+                return true;
+            } else {if ((year % 4 == 0) && (year % 100 != 0))
+                        return true;
+                    else
+                return false;
+            }
+
+        }
+    }
+}
