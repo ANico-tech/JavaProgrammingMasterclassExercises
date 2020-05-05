2730d43009a8d2916bb101a1cf17eb78b5d73182
ANico-tech <59415915+ANico-tech@users.noreply.github.com>
2020-05-05 18:06:20 +0300

    Update

--- src/OOP/BankAccount.java
+++ src/OOP/BankAccount.java
@@ -1,4 +1,64 @@
 package OOP;
 
 public class BankAccount {
+    private int accountNumber;
+    private double balance;
+    private String customerName;
+    private String email;
+    private int phoneNumber;
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void depositFunds(double extraFunds){
+        this.balance +=extraFunds;
+        System.out.println("New balance value is:" + this.balance);
+    }
+
+    public void withdrawFunds(double neededFunds){
+        if (this.balance<neededFunds){
+            System.out.println("Insufficient funds ");
+        }
+        else {
+            this.balance -=neededFunds;
+            System.out.println("New balance value is:" + this.balance);
+        }
+    }
 }
--- src/OOP/VipCustomers.java
+++ src/OOP/VipCustomers.java
@@ -1,4 +1,37 @@
 package OOP;
 
 public class VipCustomers {
+    private String name;
+    private double creditLimit;
+    private String address;
+
+    public VipCustomers() {
+        this("Default Name",100,"Default Address");
+    }
+
+    public VipCustomers(String name, String address) {
+       this("Default Name",120,"Default Address");
+    }
+
+    public VipCustomers(String name, double creditLimit, String address) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.address = address;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
 }
